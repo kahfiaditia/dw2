9d2fa81b9caecc4ea5320675bf8b514ed0830570
Gepezet <gepezet91@gmail.com>
2022-04-14 15:01:58 +0700

    3.1

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1036,26 +1036,45 @@ class EmployeeController extends Controller
                 ->groupBy('karyawan_id')
                 ->get();
             if (count($ijazah) > 0) {
-                $count = ($ijazah[0]->sd + $ijazah[0]->smp + $ijazah[0]->sma + $ijazah[0]->smk + $ijazah[0]->s1);
-                if ($count >= 4) {
+                // $count = ($ijazah[0]->sd + $ijazah[0]->smp + $ijazah[0]->sma + $ijazah[0]->smk + $ijazah[0]->s1);
+                // if ($count >= 4) {
+                if ($ijazah[0]->sd >= 1 and $ijazah[0]->smp >= 1 and ($ijazah[0]->sma + $ijazah[0]->smk) >= 1 and $ijazah[0]->s1 >= 1) {
                     $code = 200;
                 } else {
                     $code = 404;
                     $data = 'Ijazah ';
-                    if (intval($ijazah[0]->sd) === 0) {
+                    // if (intval($ijazah[0]->sd) === 0) {
+                    //     $data .= 'SD, ';
+                    // }
+                    // if (intval($ijazah[0]->smp) === 0) {
+                    //     $data .= 'SMP, ';
+                    // }
+                    // if (intval($ijazah[0]->sma) === 0 or intval($ijazah[0]->smk) === 0) {
+                    //     if ($ijazah[0]->sma === 0) {
+                    //         $data .= 'SMA, ';
+                    //     } else {
+                    //         $data .= 'SMK, ';
+                    //     }
+                    // }
+                    // if (intval($ijazah[0]->s1) === 0) {
+                    //     $data .= 'S1, ';
+                    // }
+                    // array_push($message, $data);
+
+                    if ($ijazah[0]->sd === 0) {
                         $data .= 'SD, ';
                     }
-                    if (intval($ijazah[0]->smp) === 0) {
+                    if ($ijazah[0]->smp === 0) {
                         $data .= 'SMP, ';
                     }
-                    if (intval($ijazah[0]->sma) === 0 or intval($ijazah[0]->smk) === 0) {
+                    if ($ijazah[0]->sma === 0 or $ijazah[0]->smk === 0) {
                         if ($ijazah[0]->sma === 0) {
                             $data .= 'SMA, ';
                         } else {
                             $data .= 'SMK, ';
                         }
                     }
-                    if (intval($ijazah[0]->s1) === 0) {
+                    if ($ijazah[0]->s1 === 0) {
                         $data .= 'S1, ';
                     }
                     array_push($message, $data);
