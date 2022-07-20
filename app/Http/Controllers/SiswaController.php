fabc31a130477c3c4b98e564e9cefc656cb08f66
ardiyan15 <ardhiyan15@gmail.com>
2022-07-20 13:53:19 +0700

    remove unused import

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -20,7 +20,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Crypt;
 use Maatwebsite\Excel\Facades\Excel;
-use Illuminate\Support\Facades\Storage;
 
 class SiswaController extends Controller
 {
