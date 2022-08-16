06d82274b295ea8b890884f354d1abc874f4a5c0
Gepezet <gepezet91@gmail.com>
2022-08-16 11:09:38 +0700

    update seeder menu dan sub menu

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -23,61 +23,85 @@ class MenuSeeder extends Seeder
                 'icon' => 'bx bxs-dashboard',
                 'sub_menu' => '0',
                 'order_menu' => '1',
+                'display' => '1',
             ], [
-                'menu' => 'Data',
+                'menu' => 'Karyawan',
                 'icon' => 'bx bx-group',
                 'sub_menu' => '1',
                 'order_menu' => '2',
+                'display' => '1',
             ], [
                 'menu' => 'Akun',
                 'icon' => 'bx bx-group',
                 'sub_menu' => '0',
                 'order_menu' => '4',
+                'display' => '1',
             ], [
                 'menu' => 'Agama',
                 'icon' => 'bx bx-list-ul',
                 'sub_menu' => '0',
                 'order_menu' => '6',
+                'display' => '1',
             ], [
                 'menu' => 'Kodepos',
                 'icon' => 'bx bx-directions',
                 'sub_menu' => '0',
                 'order_menu' => '7',
+                'display' => '1',
             ], [
                 'menu' => 'Siswa',
                 'icon' => 'bx bx-group',
                 'sub_menu' => '0',
                 'order_menu' => '3',
+                'display' => '1',
             ], [
                 'menu' => 'Kebutuhan Khusus',
                 'icon' => 'bx bx-accessibility',
                 'sub_menu' => '0',
                 'order_menu' => '8',
+                'display' => '1',
             ], [
                 'menu' => 'Tagihan',
                 'icon' => 'bx bx-receipt',
                 'sub_menu' => '0',
                 'order_menu' => '10',
+                'display' => '1',
             ], [
                 'menu' => 'Kelas',
                 'icon' => 'bx bxs-school',
                 'sub_menu' => '0',
                 'order_menu' => '9',
+                'display' => '1',
             ], [
                 'menu' => 'Pembayaran',
                 'icon' => 'bx bx-wallet',
                 'sub_menu' => '0',
                 'order_menu' => '12',
+                'display' => '1',
             ], [
                 'menu' => 'Setting Pembayaran',
                 'icon' => 'bx bx-wallet-alt',
                 'sub_menu' => '0',
                 'order_menu' => '11',
+                'display' => '1',
             ], [
                 'menu' => 'Hak Akses',
                 'icon' => 'bx bx-select-multiple',
                 'sub_menu' => '0',
                 'order_menu' => '5',
+                'display' => '1',
+            ], [
+                'menu' => 'Setting Website',
+                'icon' => 'bx bxl-jsfiddle text-info',
+                'sub_menu' => '1',
+                'order_menu' => '5',
+                'display' => '1',
+            ], [
+                'menu' => 'Setting',
+                'icon' => 'bx bx-cog text-info',
+                'sub_menu' => '1',
+                'order_menu' => '5',
+                'display' => '1',
             ]
         ];
         DB::table('menu')->insert($menus);
--- database/seeders/SubMenuSeeder.php
+++ database/seeders/SubMenuSeeder.php
@@ -21,13 +21,13 @@ class SubMenuSeeder extends Seeder
             [
                 'submenu' => 'Dashboard', 'route_submenu' => 'dashboard', 'type_menu' => 'view', 'menu_id' => '1',
             ], [
-                'submenu' => 'Karyawan', 'route_submenu' => 'employee', 'type_menu' => 'view', 'menu_id' => '2',
+                'submenu' => 'Data Pribadi', 'route_submenu' => 'employee', 'type_menu' => 'view', 'menu_id' => '2',
             ], [
-                'submenu' => 'Karyawan', 'route_submenu' => 'employee', 'type_menu' => 'insert', 'menu_id' => '2',
+                'submenu' => 'Data Pribadi', 'route_submenu' => 'employee', 'type_menu' => 'insert', 'menu_id' => '2',
             ], [
-                'submenu' => 'Karyawan', 'route_submenu' => 'employee', 'type_menu' => 'edit', 'menu_id' => '2',
+                'submenu' => 'Data Pribadi', 'route_submenu' => 'employee', 'type_menu' => 'edit', 'menu_id' => '2',
             ], [
-                'submenu' => 'Karyawan', 'route_submenu' => 'employee', 'type_menu' => 'delete', 'menu_id' => '2',
+                'submenu' => 'Data Pribadi', 'route_submenu' => 'employee', 'type_menu' => 'delete', 'menu_id' => '2',
             ], [
                 'submenu' => 'Absensi', 'route_submenu' => '', 'type_menu' => 'view', 'menu_id' => '2',
             ], [
@@ -39,21 +39,21 @@ class SubMenuSeeder extends Seeder
             ], [
                 'submenu' => 'Akun', 'route_submenu' => 'akun.index', 'type_menu' => 'delete', 'menu_id' => '3',
             ], [
-                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'view', 'menu_id' => '4',
+                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'insert', 'menu_id' => '4',
+                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'insert', 'menu_id' => '14',
             ], [
-                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'edit', 'menu_id' => '4',
+                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'edit', 'menu_id' => '14',
             ], [
-                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'delete', 'menu_id' => '4',
+                'submenu' => 'Agama', 'route_submenu' => 'agama', 'type_menu' => 'delete', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'view', 'menu_id' => '5',
+                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'insert', 'menu_id' => '5',
+                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'insert', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'edit', 'menu_id' => '5',
+                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'edit', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'delete', 'menu_id' => '5',
+                'submenu' => 'Kodepos', 'route_submenu' => 'kodepos', 'type_menu' => 'delete', 'menu_id' => '14',
             ], [
                 'submenu' => 'Siswa', 'route_submenu' => 'siswa.index', 'type_menu' => 'view', 'menu_id' => '6',
             ], [
@@ -63,29 +63,29 @@ class SubMenuSeeder extends Seeder
             ], [
                 'submenu' => 'Siswa', 'route_submenu' => 'siswa.index', 'type_menu' => 'delete', 'menu_id' => '6',
             ], [
-                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'view', 'menu_id' => '7',
+                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'insert', 'menu_id' => '7',
+                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'insert', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'edit', 'menu_id' => '7',
+                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'edit', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'delete', 'menu_id' => '7',
+                'submenu' => 'Kebutuhan Khusus', 'route_submenu' => 'needs.index', 'type_menu' => 'delete', 'menu_id' => '14',
             ], [
-                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'view', 'menu_id' => '8',
+                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'insert', 'menu_id' => '8',
+                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'insert', 'menu_id' => '14',
             ], [
-                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'edit', 'menu_id' => '8',
+                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'edit', 'menu_id' => '14',
             ], [
-                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'delete', 'menu_id' => '8',
+                'submenu' => 'Tagihan', 'route_submenu' => 'bills.index', 'type_menu' => 'delete', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'view', 'menu_id' => '9',
+                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'insert', 'menu_id' => '9',
+                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'insert', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'edit', 'menu_id' => '9',
+                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'edit', 'menu_id' => '14',
             ], [
-                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'delete', 'menu_id' => '9',
+                'submenu' => 'Kelas', 'route_submenu' => 'classes.index', 'type_menu' => 'delete', 'menu_id' => '14',
             ], [
                 'submenu' => 'Pembayaran', 'route_submenu' => 'invoice.index', 'type_menu' => 'view', 'menu_id' => '10',
             ], [
@@ -95,17 +95,41 @@ class SubMenuSeeder extends Seeder
             ], [
                 'submenu' => 'Pembayaran', 'route_submenu' => 'invoice.index', 'type_menu' => 'delete', 'menu_id' => '10',
             ], [
-                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'view', 'menu_id' => '11',
+                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'insert', 'menu_id' => '11',
+                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'insert', 'menu_id' => '14',
             ], [
-                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'edit', 'menu_id' => '11',
+                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'edit', 'menu_id' => '14',
             ], [
-                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'delete', 'menu_id' => '11',
+                'submenu' => 'Setting Pembayaran', 'route_submenu' => 'payment.index', 'type_menu' => 'delete', 'menu_id' => '14',
             ], [
-                'submenu' => 'Hak Akses', 'route_submenu' => 'primession.index', 'type_menu' => 'view', 'menu_id' => '12',
+                'submenu' => 'Hak Akses', 'route_submenu' => 'primession.index', 'type_menu' => 'view', 'menu_id' => '14',
             ], [
-                'submenu' => 'Hak Akses', 'route_submenu' => 'primession.index', 'type_menu' => 'insert', 'menu_id' => '12',
+                'submenu' => 'Hak Akses', 'route_submenu' => 'primession.index', 'type_menu' => 'insert', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Maintenance', 'route_submenu' => 'setting.index', 'type_menu' => 'view', 'menu_id' => '13',
+            ], [
+                'submenu' => 'Menu', 'route_submenu' => null, 'type_menu' => 'view', 'menu_id' => '13',
+            ], [
+                'submenu' => 'Sub Menu', 'route_submenu' => null, 'type_menu' => 'view', 'menu_id' => '13',
+            ], [
+                'submenu' => 'Setting Diskon', 'route_submenu' => 'diskon.index', 'type_menu' => 'view', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Setting Diskon', 'route_submenu' => 'diskon.index', 'type_menu' => 'insert', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Setting Diskon', 'route_submenu' => 'diskon.index', 'type_menu' => 'edit', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Setting Diskon', 'route_submenu' => 'diskon.index', 'type_menu' => 'delete', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Siswa Prestasi', 'route_submenu' => 'prestasi.index', 'type_menu' => 'view', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Siswa Prestasi', 'route_submenu' => 'prestasi.index', 'type_menu' => 'insert', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Siswa Prestasi', 'route_submenu' => 'prestasi.index', 'type_menu' => 'edit', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Siswa Prestasi', 'route_submenu' => 'prestasi.index', 'type_menu' => 'delete', 'menu_id' => '14',
+            ], [
+                'submenu' => 'Rubah Pembayaran Siswa', 'route_submenu' => 'siswa.index', 'type_menu' => 'edit', 'menu_id' => '6',
             ]
         ];
         DB::table('submenu')->insert($sub_menus);
